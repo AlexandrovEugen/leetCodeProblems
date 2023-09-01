a79b94185516985256c26be3e3a663c37f2a0f36
EYAleksandrov <evgeny.alexandrov1994@gmail.com>
2023-09-01 17:24:13 +0200

    2 hard daily challenge is solved

---
+++ src/test/java/com/eval/hard/minimumNumberOfTapsToOpenToWaterAGarden/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.eval.hard.minimumNumberOfTapsToOpenToWaterAGarden;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void exampleFromLeetCode1(){
+
+        var n = 5;
+        var ranges = new int[]{3, 4, 1, 1, 0, 0};
+
+        val minimumNumberOfTaps = solution.minTaps(n, ranges);
+
+        assertThat(minimumNumberOfTaps).isEqualTo(1);
+    }
+    @Test
+    void exampleFromLeetCode2(){
+
+        var n = 3;
+        var ranges = new int[]{0, 0, 0, 0};
+
+        val minimumNumberOfTaps = solution.minTaps(n, ranges);
+
+        assertThat(minimumNumberOfTaps).isEqualTo(-1);
+    }
+
+
+
+
+}
